760fff0df5c2dd1784818ec497e87464848cc11d
Daniil <kadaniilo@gmail.com>
2018-10-09 12:46:19 +0300

    Adds codeYourMethods

---
+++ codeYourMethods.js
@@ -0,0 +1,28 @@
+function myForEach{
+
+}
+
+function myMap() {
+
+}
+
+function myFilter() {
+
+}
+
+function myReduce() {
+
+}
+
+function myFind() {
+
+}
+
+function myEvery() {
+
+}
+
+function mySome() {
+
+}
+
