27c2d8f36a84e7ee1cefe40b33ede2f08c7cc5e9
George Babayan <ggbabayan@gmail.com>
2019-01-28 11:06:52 +0300

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,17 +1,17 @@
-let oddNumbers = function(nums) {
-  let odds = [];
+function oddNumbers(nums) {
+  let odds = []
   for (let i = 0; i < nums.length; i++) {
-    if ((nums[i] % 2) == 1) {
+    if ((nums[i] % 2) === 1) {
       odds.push(nums[i]);
     }
   }
   return odds
-};
+}
 
 
 function longStrings(strings, minimumLength = 0){
   let longs = []
-  for (var i = 0; i < strings.length; i++) {
+  for (let i = 0; i < strings.length; i++) {
     if (strings[i].length >= minimumLength)
     longs.push(strings[i])
   }
