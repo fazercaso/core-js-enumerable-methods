762185e333f22850b47ce08d02b5b18328e20ff5
Максимилиан Андриянов <fazercaso@gmail.com>
2022-06-07 18:12:47 +0300

    Release 4

--- aggregate.js
+++ aggregate.js
@@ -1,6 +1,10 @@
-function sum(arrayNumbers, sum = 0){
-  for (let i = 0; i < arrayNumbers.length; i++ ){
-    sum += arrayNumbers[i];
-  }
-  return sum
-}
+// function sum(arrayNumbers, sum = 0){
+//   // for (let i = 0; i < arrayNumbers.length; i++ ){
+//   //   sum += arrayNumbers[i];
+//   // }
+//   return sum
+// }
+const sum = 13
+const arrayNumbers = [2,4,5,34,65,68,967]
+const s = arrayNumbers.reduce((arrayNumbers, summ) => arrayNumbers + summ,sum)
+console.log(s)
